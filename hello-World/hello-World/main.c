768d3a341344691c5aa5d9630b6199085f0525d0
Michael Esch <michaelesch@michaels-mbp.fritz.box>
2018-09-23 18:34:02 +0200

    Files generated by Xcode.

---
+++ hello-World/hello-World/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello-World
+//
+//  Created by Michael Esch on 23.09.18.
+//  Copyright © 2018 Michael Esch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
